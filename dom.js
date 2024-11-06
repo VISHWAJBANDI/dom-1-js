3c339a7c6815aad05ee30c02d090fc8b1a809541
unknown <vishwajpatel143@gmail.com>
2024-11-06 10:23:52 +0530

    dom-1 created

---
+++ dom.js
@@ -0,0 +1,19 @@
+
+let b=document.getElementById("btn")
+
+b.style.background="blue"
+let c=document.getElementById("p")
+function a(){
+    p.innerHTML=`<img src="image-01.jpg"  style="border-radius: 30px;" width="200px" />;
+    <img src="image-02.jpg"  style="border-radius: 100px;" width="200px" />
+     <img src="image-03.jpg"  style="border-radius: 50%;" width="200px" />`
+}
+
+const clickimage=document.getElementById("btn2")
+const clickhere=document.getElementById("imageshere")
+function imagefn(){
+    imageshere.innerHTML=
+    `<span style="background-color:skyblue; font-size: 60px; border-radius: 50%">hi! i am clicked by button!"</span>`
+    
+
+}
